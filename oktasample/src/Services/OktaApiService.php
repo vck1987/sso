aa59b34a1f80327bd42835a45046342596380ec7
vck1987 <72908153+vck1987@users.noreply.github.com>
2021-01-31 21:12:58 +0530

    Add files via upload

--- oktasample/src/Services/OktaApiService.php
+++ oktasample/src/Services/OktaApiService.php
@@ -1,1 +1,150 @@
+<?php
+namespace Src\Services;
 
+class OktaApiService
+{
+    private $clientId;
+    private $clientSecret;
+    private $redirectUri;
+    private $metadataUrl;
+    private $apiToken;
+    private $apiUrlBase;
+
+    public function __construct()
+    {
+        $this->clientId     = getenv('CLIENT_ID');
+        $this->clientSecret = getenv('CLIENT_SECRET');
+        $this->redirectUri  = getenv('REDIRECT_URI');
+        $this->metadataUrl  = getenv('METADATA_URL');
+        $this->apiToken     = getenv('API_TOKEN');
+        $this->apiUrlBase   = getenv('API_URL_BASE');
+    }
+
+    public function buildAuthorizeUrl($state)
+    {
+        $metadata = $this->httpRequest($this->metadataUrl);
+        $url = $metadata->authorization_endpoint . '?' . http_build_query([
+            'response_type' => 'code',
+            'client_id' => $this->clientId,
+            'redirect_uri' => $this->redirectUri,
+            'state' => $state,
+            'scope' => 'openid',
+        ]);
+        return $url;
+    }
+
+    public function authorizeUser()
+    {
+        
+        if ($_SESSION['state'] != $_GET['state']) {
+            $result['error'] = true;
+            $result['errorMessage'] = 'Authorization server returned an invalid state parameter';
+            return $result;
+        }
+
+        if (isset($_GET['error'])) {
+            $result['error'] = true;
+            $result['errorMessage'] = 'Authorization server returned an error: '.htmlspecialchars($_GET['error']);
+            return $result;
+        }
+
+        $metadata = $this->httpRequest($this->metadataUrl);
+
+        $response = $this->httpRequest($metadata->token_endpoint, [
+            'grant_type' => 'authorization_code',
+            'code' => $_GET['code'],
+            'redirect_uri' => $this->redirectUri,
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret
+        ]);
+
+        if (! isset($response->access_token)) {
+            $result['error'] = true;
+            $result['errorMessage'] = 'Error fetching access token!';
+            return $result;
+        }
+        $_SESSION['access_token'] = $response->access_token;
+
+        $token = $this->httpRequest($metadata->introspection_endpoint, [
+            'token' => $response->access_token,
+            'client_id' => $this->clientId,
+            'client_secret' => $this->clientSecret
+        ]);
+
+        if ($token->active == 1) {
+            $_SESSION['username'] = $token->username;
+            $result['success'] = true;
+            return $result;
+        }
+    }
+
+    public function registerUser($input)
+    {
+        $data['profile'] = [
+            'firstName' => $input['first_name'],
+            'lastName'  => $input['last_name'],
+            'email'     => $input['email'],
+            'login'     => $input['email']
+        ];
+        $data['credentials'] = [
+            'password'  => [
+                'value' => $input['password']
+            ]
+        ];
+        $data = json_encode($data);
+
+        $ch = curl_init($this->apiUrlBase . 'users');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data),
+            'Authorization: SSWS ' . $this->apiToken
+        ]);
+
+        return curl_exec($ch);
+    }
+
+    public function findUser($input)
+    {
+        $url = $this->apiUrlBase . 'users?q=' . urlencode($input['email']) . '&limit=1';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Authorization: SSWS ' . $this->apiToken
+        ]);
+
+        return curl_exec($ch);
+    }
+
+    public function resetPassword($userId)
+    {
+        $url = $this->apiUrlBase . 'users/' . $userId . '/lifecycle/reset_password';
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, []);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Authorization: SSWS ' . $this->apiToken
+        ]);
+
+        return curl_exec($ch);
+    }
+
+    private function httpRequest($url, $params = null)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($params) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
+        return json_decode(curl_exec($ch));
+    }
+}
