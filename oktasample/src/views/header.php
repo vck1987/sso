4ab01e0c93e54e903bc2a213caba3dfacd5fc26c
vck1987 <72908153+vck1987@users.noreply.github.com>
2021-01-31 21:14:19 +0530

    Add files via upload

---
+++ oktasample/src/views/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="PHP Login App bd-index-custom-example">
+        <title>Basic PHP Okta Login Example </title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    </head>
+    <body class="layout-default" align="center">
+        <nav id="navbar" class="navbar has-shadow is-spaced">
+            <div class="container">
+            <div class="content">Basic PHP Okta Login Example
+                <h1>Basic PHP Okta Login Example</h1>
+                <?php
+                    if (isset($_SESSION['username'])) {
+                ?>
+                        <p>
+                            Logged in as <?php echo $_SESSION['username'] ?>
+                        </p>
+                        <p><a href="/?logout">Log Out</a></p>
+                <?php 
+                    } else {
+                ?>
+                        <p>Not logged in</p>
+                        <p><a href="?login">Log In</a> <!--| <a href="?register">Register</a>--></p>
+                <?php
+                    }
+                ?>
+            </div>
+            </div>
+        </nav>
+        
